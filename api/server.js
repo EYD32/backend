29e5844d98d8f50dd95ad18e1b9841e0ae8da5e8
Richard Perry <85124966+rdperry031@users.noreply.github.com>
2023-03-05 23:47:25 -0600

    implemented cors

--- api/server.js
+++ api/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
-
+const cors = require('cors')
 
 const server = express()
 server.use(express.json())
+server.use(cors())
 
 const macrosRouter = require('./macros/macros-router')
 
