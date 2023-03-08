7cfe4d13c7a70d26217b2cf4269f4b3b78feb36f
Richard Perry <85124966+rdperry031@users.noreply.github.com>
2023-03-07 21:06:37 -0600

    implemented endpoint for adding new users

--- api/user/user-model.js
+++ api/user/user-model.js
@@ -3,12 +3,20 @@ const db = require('../data/db-config')
 function getUser(){
     return db('user')
 }
+async function newUser(user){
+    const newUser = await db('user').insert(user, ['user_id', 'firstName'])
+    return newUser
+} 
+
+
 
 function updateUser(id, changes){
     return db('user').where({id}).update(changes)
 }
 
+
 module.exports = {
     getUser,
+    newUser,
     updateUser
 }
--- api/user/user-router.js
+++ api/user/user-router.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
 
+const res = require('express/lib/response');
 const User = require('./user-model');
 
 router.get('/', async (req, res, next) => {
     try{
         const user = await User.getUser();
         res.status(200).json(user)
+    }catch(err){
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try{
+        const newUser = await User.newUser(req.body)
+        res.status(201).json(newUser)
     }catch(err){
         next(err)
     }
